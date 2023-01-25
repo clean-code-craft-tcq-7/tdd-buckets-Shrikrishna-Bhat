0b23ab5bfc54c163667550c177d7ab07490aa662
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2023-01-25 19:44:54 +0530

    Update Charging.c

--- Charging.c
+++ Charging.c
@@ -22,4 +22,24 @@ int getPeriodicCharingSample()
   {
     scanf("%d",chargingSamples[i]);
   }
+  
+  BubbleSort(chargingSamples, NumOfChargingSamples);
+}
+
+void BubbleSort(int chargingSamples[], int NumCS)
+{
+  int i,j;
+  for(i=0;i<NumCS;i++)
+  {
+    for(j=0;j<(NumCS - i - 1);j++)
+    {
+      if(chargingSamples[j]>chargingSamples[j+1])
+      {
+        int temp;             
+        temp                  = chargingSamples[j];
+        chargingSamples[j]    = chargingSamples[j+1];
+        chargingSamples[j+1]  = temp;
+      }
+    }
+  }
 }
